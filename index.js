8c6155eefdcfce2c07e5b19d1417a2d924b105bf
tiffyy101 <bobatiffy101@gmail.com>
2022-11-09 10:16:22 -0500

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const DOMSelctors = {
     fruit: document.getElementById("fruit"),
-    color: document.getElementById("color"),
-    iceCream: document.getElementById("iceCream"),
-    animal: document.getElementById("animal"),
-};
+}
