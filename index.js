fcd1c8b821c0d1d30a7f07d4bdd763db2b157335
tiffyy101 <bobatiffy101@gmail.com>
2022-11-10 10:14:25 -0500

    3rd commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const DOMSelctors = {
-    fruit: document.getElementById("fruit"),
-}
+    button: document.getElementById("btn"),
+    box: document.getElementById("container-box"),
+    input: document.querySelector('#input'),
+};
+
+DOMSelectors.button.addEventListener("click", function(){
+    let input = DOMSelectors.input.value;
+    DOMSelectors.box.insertAdjacentHTML("beforehand", '<p>${input}</p>');
+    DOMSelectors.input.value = "";
+});
