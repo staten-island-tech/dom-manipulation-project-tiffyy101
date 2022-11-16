06ee1b955515c335cce0f8085b1255512d05dda6
tiffyy101 <bobatiffy101@gmail.com>
2022-11-16 10:14:54 -0500

    7th commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const DOMSelectors = {
     button: document.getElementById("button"),
-    box: document.getElementById("pink-box"),
+    label: document.getElementById("name"),
     input: document.getElementById("fruit"),
     list: document.querySelectorAll(".list")
 };
 
+console.log(DOMSelectors.input);
+console.log(DOMSelectors.button);
+console.log(DOMSelectors.label);
+console.log(DOMSelectors.list);
