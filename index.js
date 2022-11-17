039842c0d10537351877e434488823d27fea483a
tiffyy101 <bobatiffy101@gmail.com>
2022-11-17 10:14:34 -0500

    7th commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const DOMSelectors = {
     button: document.getElementById("button"),
     label: document.getElementById("name"),
-    input: document.getElementById("fruit"),
-    list: document.querySelectorAll(".list")
+    input: document.getElementById("text"),
+    list: document.querySelectorAll(".list"),
+    output: document.getElementById("output")
 };
 
 console.log(DOMSelectors.input);
 console.log(DOMSelectors.button);
 console.log(DOMSelectors.label);
 console.log(DOMSelectors.list);
+
+const text = document.getElementById("text");
+const output = document.getElementById("output");
+
+text.addEventListener('submit', (event) => output.textContent = `You pressed "${event.key}".`);
+
+function backgroundandText (background, text) {
+    background.style.backgroundColor = "yellow";
+    text.style.fontSize = "20px";
+}
+
+DOMSelectors.form.addEventListener('submit', function() {
+    document.textContent.style.color = 'brown';
+});
