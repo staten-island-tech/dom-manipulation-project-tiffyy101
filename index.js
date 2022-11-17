326125778c764e9c40f9a767ae88801f2ef2e9d4
tiffyy101 <bobatiffy101@gmail.com>
2022-11-17 17:48:45 -0500

    :'D

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const DOMSelectors = {
-    button: document.getElementById("button"),
-    label: document.getElementById("name"),
-    input: document.getElementById("text"),
-    list: document.querySelectorAll(".list"),
-    output: document.getElementById("output")
+    box: document.getElementById("box"),
+    button: document.getElementById("btn"),
+    fruit: document.getElementById("fruit"),
+    iceCream: document.getElementById("iceCream"),
+    drink: document.getElementById("drink"),
+    url: document.getElementById("url"),
 };
-
-console.log(DOMSelectors.input);
-console.log(DOMSelectors.button);
-console.log(DOMSelectors.label);
-console.log(DOMSelectors.list);
-
-const text = document.getElementById("text");
-const output = document.getElementById("output");
-
-text.addEventListener('submit', (event) => output.textContent = `You pressed "${event.key}".`);
-
-function backgroundandText (background, text) {
-    background.style.backgroundColor = "yellow";
-    text.style.fontSize = "20px";
-}
-
-DOMSelectors.form.addEventListener('submit', function() {
-    document.textContent.style.color = 'brown';
-});
+console.log(DOMSelectors);
