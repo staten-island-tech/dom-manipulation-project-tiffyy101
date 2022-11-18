16747a4e0ea52af9b5f64343dea38e9556427aba
tiffyy101 <bobatiffy101@gmail.com>
2022-11-18 15:43:06 -0500

    :c

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const DOMSelectors = {
+    text: document.getElementById("text"),
     form: document.getElementById("form"),
     box: document.getElementById("box"),
     button: document.getElementById("btn"),
-    fruit: document.getElementById("fruit"),
-    iceCream: document.getElementById("iceCream"),
-    drink: document.getElementById("drink"),
+    flavor: document.getElementById("flavor"),
+    topping1: document.getElementById("topping1"),
+    topping2: document.getElementById("topping2"),
     url: document.getElementById("url"),
 };
 console.log(DOMSelectors);
@@ -12,7 +13,22 @@ console.log(DOMSelectors);
 function background(background){
     background.style.backgroundColor = "lightblue";
 }
+    background(DOMSelectors.button);
 
-DOMSelectors.form.addEventListener("submit", function(submit){
-    background(DOMSelectors.button),
-})
+DOMselectors.form.addEventListener("submit", function (event) {
+    event.preventDefault(event);
+    input();
+  });
+  function input() {
+    DOMselectors.display.insertAdjacentHTML(
+      "beforeend",
+  
+      `<div class="display-card">
+          <img class="display-img" src=${DOMselectors.flavor.value} />
+          <h2 class="display-artist">${DOMselectors.topping1.value}</h2>
+          <h3 class="display-album">${DOMselectors.topping2.value}</h3>
+          <h4 class="display-album">${DOMselectors.url.value}</h4>
+          <button class="remove btn">remove</button>
+        </div>`
+    );
+  } 
