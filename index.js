1978940a970fc74512ad23bdfba2e8a13fc7bd8f
tiffyy101 <bobatiffy101@gmail.com>
2022-11-19 14:11:36 -0500

    ?

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const DOMSelectors = {
-    text: document.getElementById("text"),
-    form: document.getElementById("form"),
-    box: document.getElementById("box"),
-    button: document.getElementById("btn"),
-    flavor: document.getElementById("flavor"),
-    topping1: document.getElementById("topping1"),
-    topping2: document.getElementById("topping2"),
-    url: document.getElementById("url"),
+  text: document.getElementById("text"),
+  form: document.getElementById("form"),
+  box: document.getElementById("box"),
+  button: document.getElementById("btn"),
+  flavor: document.getElementById("flavor"),
+  topping1: document.getElementById("topping1"),
+  topping2: document.getElementById("topping2"),
+  url: document.getElementById("url"),
+  display: document.getElementById("display"),
 };
 console.log(DOMSelectors);
 
-function background(background){
-    background.style.backgroundColor = "lightblue";
-}
-    background(DOMSelectors.button);
-
-DOMselectors.form.addEventListener("submit", function (event) {
-    event.preventDefault(event);
-    input();
-  });
-  function input() {
-    DOMselectors.display.insertAdjacentHTML(
-      "beforeend",
-  
-      `<div class="display-card">
-          <img class="display-img" src=${DOMselectors.flavor.value} />
-          <h2 class="display-artist">${DOMselectors.topping1.value}</h2>
-          <h3 class="display-album">${DOMselectors.topping2.value}</h3>
-          <h4 class="display-album">${DOMselectors.url.value}</h4>
-          <button class="remove btn">remove</button>
-        </div>`
+DOMSelectors.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let flavor = DOMSelectors.flavor.value;
+  let topping1 = DOMSelectors.topping1.value;
+  let topping2 = DOMSelectors.topping2.value;
+  let url = DOMSelectors.url.value;
+  console.log(DOMSelectors);
+  creation();
+  clear();
+  function creation() {
+    DOMSelectors.button.insertAdjacentHTML(
+      `afterend`,
+      `<div class="iceCreamMaker">
+      <h2>You have created a ${flavor} ice cream with ${topping1} and ${topping2}!</h2>
+      <h3>${url}</h3>
+       <button class="clear">Clear</button>
+  </div>`
     );
-  } 
+    DOMSelectors.flavor.value = "";
+    DOMSelectors.topping1.value = "";
+    DOMSelectors.topping2.value = "";
+  }
+})
